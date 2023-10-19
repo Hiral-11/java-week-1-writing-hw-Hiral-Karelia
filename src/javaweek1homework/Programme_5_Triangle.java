c426bc63edc8c5671109812e4b18e14267e49103
Hiral-11 <hiral.karelia1@gmail.com>
2023-10-19 23:03:22 +0100

    homework hiral karelia

---
+++ src/javaweek1homework/Programme_5_Triangle.java
@@ -0,0 +1,23 @@
+package javaweek1homework;
+
+/**
+ * 5. Write a Java rogram to print below Traingle
+ *           *
+ *          * *
+ *         * * *
+ *        * * * *
+ *       * * * * *
+ *      * * * * * *
+ */
+
+public class Programme_5_Triangle {
+    public static void main(String[] args) {
+        System.out.println("     *     ");
+        System.out.println("    * *     ");
+        System.out.println("   * * *    ");
+        System.out.println("  * * * *   ");
+        System.out.println(" * * * * *  ");
+        System.out.println("* * * * * * ");
+
+    }
+}
